78ee0d9759a31ce4b5eb6cabbeb47c59a3365d2f
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-09-05 17:26:58 -0700

    handlebars works now

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 var express = require("express");
-var mysql = require("mysql");
 var exphbs = require("express-handlebars");
 
 var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var route = require('./routes/routes.js');
+
+app.use(route);
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
